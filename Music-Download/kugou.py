2fa202f97f9d68bdb35fd62d9641bdb6587d5943
Ken-lxy <38093104+Ken-lxy@users.noreply.github.com>
2018-06-04 22:23:04 +0800

    Update kugou.py

--- Music-Download/kugou.py
+++ Music-Download/kugou.py
@@ -1,16 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 23 09:34:42 2018
+# -*- coding: utf-8 
 
-@author: tarena
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 22 12:14:55 2018
-
-@author: ken
-"""
 from selenium import webdriver
 from time import sleep
 from selenium.common.exceptions import NoSuchElementException
@@ -18,28 +7,26 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import ActionChains
 import urllib
 import sys
+
 mname = ''
 
+
+#获取音乐的URL
 def get_url(url,key):
     driver = webdriver.Chrome()
-#    url = input('输入连接地址')
     driver.get(url)
-#    sleep(1)
     driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div[1]/input').send_keys(key)
     driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div[1]/div/i').click()
-    
     result_url = driver.current_url
-    print(result_url)
     driver.close()
     driver.quit()
     return result_url
 
+
 def show_results(url):
     driver = webdriver.Chrome()
-#    driver.get('http://www.kugou.com/yy/html/search.html#searchType=song&searchKeyWord=%E5%91%A8%E6%9D%B0%E4%BC%A6')
     driver.get(url)
-#    sleep(1)
-    for i in range(1,1000):
+    for i in range(1,1000):  #假设有一千首歌歌曲
         try:
             print('%s'%i+driver.find_element_by_xpath('//*[@id="search_song"]/div[2]/ul[2]/li[%s]/div[1]/a'%i).get_attribute('title'))
         except NoSuchElementException as e:
@@ -52,18 +39,20 @@ def show_results(url):
         global mname
         mname = driver.find_element_by_xpath('//*[@id="search_song"]/div[2]/ul[2]/li[%s]/div[1]/a'%choice).get_attribute('title')
         a = driver.find_element_by_xpath('//*[@id="search_song"]/div[2]/ul[2]/li[%s]/div[1]/a'%choice)
-        actions =ActionChains(driver)
+        actions =ActionChains(driver)  #创建一个实例来模拟鼠标
         actions.move_to_element(a)
         actions.click(a)
         actions.perform()
         sleep(1)
         handlers = driver.window_handles
-        driver.switch_to.window(handlers[1])
+        driver.switch_to.window(handlers[1])  #跳转到下一个页面
         result = driver.find_element_by_xpath('//*[@id="myAudio"]').get_attribute('src')
         driver.close()
         driver.quit()
     return result
 
+
+#回调函数
 def cbk(a,b,c):
     per = 100.0*a*b/c
     if per>100:
@@ -82,11 +71,11 @@ def choice(url):
             urllib.request.urlretrieve(result,local,cbk)
             print('\n\n')
             print('finish down %s.mp3'%mname+'\n\n')  
+            
+            
 def main():
-#    sleep(1)
     while True:
         key = input('请输入关键字 ')
-#        url = input('请输入链接地址')
         url = 'http://www.kugou.com'
         if not key:
             choice(url)
